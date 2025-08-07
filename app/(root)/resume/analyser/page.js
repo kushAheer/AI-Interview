8473f4a3b3afec79b5d4f4e89c2d8bfe0535f1d8
kushAheer <kushaheer709@gmail.com>
2025-08-07 17:48:56 +0530

    Build Error Fixed

--- app/(root)/resume/analyser/page.js
+++ app/(root)/resume/analyser/page.js
@@ -1,16 +1,23 @@
 import React from "react";
 import FileUploader from "@/components/FileUploader";
 import { getCurrentUser } from "@/lib/actions/auth.action";
+import { redirect } from "next/navigation";
+
 
 async function page() {
   const user = await getCurrentUser();
 
+  if (!user) {
+    redirect("/sign-in");
+  }
+
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="text-2xl font-bold mb-4">Resume Analyser</h1>
-      <FileUploader userId={user.uid} />
+      <FileUploader userId={user?.uid} />
     </div>
   );
 }
 
 export default page;
+export const dynamic = 'force-dynamic';
